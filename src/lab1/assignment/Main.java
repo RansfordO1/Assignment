cd65b127a4db36e146be2fbe2f3f36b7c28aadc8
Ransford <300621813448@bluecrest.edu.gh>
2020-05-10 15:04:40 +0000

    Initial commit: MovieQuoteInfo classes

---
+++ src/lab1/assignment/Main.java
@@ -0,0 +1,11 @@
+package lab1.assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+      MovieQuoteInfo nMovie = new MovieQuoteInfo();
+
+      nMovie.movieInfo();
+
+    }
+}
---
+++ src/lab1/assignment/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+package lab1.assignment;
+
+public class MovieQuoteInfo {
+
+    public void movieInfo() {
+        String nCharacter = " Tom Cruise ";
+        int nYear = 2018;
+        String nName = "Mission Impossible Fallout";
+        String nQuote = " ' We'll Burn That Bridge When We Get To It.' ";
+
+        System.out.println(" My favourite  movie quote is  "+ nQuote + " \n " + " said by "+ nCharacter +" \n " + " From the movie: "+ nName +" \n " + " In the year "+ nYear);
+    }
+}
